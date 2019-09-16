1730532cda70812221eb444a0ab6f33e958a2c2c
Juan Carlos Frutos <juancarlos.frutos@elparking.com>
2019-09-16 15:03:45 +0200

    Draft Box, Flex rules

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const allRules = {
   'no-unknown-theme-color': require('./lib/rules/no-unknown-theme-color'),
   'no-unknown-theme-font-size': require('./lib/rules/no-unknown-theme-font-size'),
   'no-unknown-theme-font-weight': require('./lib/rules/no-unknown-theme-font-weight'),
+  'box-attrb-validator': require('./lib/rules/box-attrb-validator.js'),
+  'flex-attrb-validator': require('./lib/rules/flex-attrb-validator.js'),
 }
 
 module.exports = {
@@ -19,6 +21,8 @@ module.exports = {
         'theme-helpers/no-unknown-theme-color': 2,
         'theme-helpers/no-unknown-theme-font-size': 2,
         'theme-helpers/no-unknown-theme-font-weight': 2,
+        'theme-helpers/box-attrb-validator': 2,
+        'theme-helpers/flex-attrb-validator': 2,
       },
     },
   },
