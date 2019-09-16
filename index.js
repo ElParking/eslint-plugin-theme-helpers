23ae3a9292ba7022344285f034f0cea4cda91efc
Juan Carlos Frutos <juancarlos.frutos@elparking.com>
2019-09-16 08:07:04 +0200

    New rules for font size and weight

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const allRules = {
   'no-unknown-theme-color': require('./lib/rules/no-unknown-theme-color'),
+  'no-unknown-theme-font-size': require('./lib/rules/no-unknown-theme-font-size'),
+  'no-unknown-theme-font-weight': require('./lib/rules/no-unknown-theme-font-weight'),
 }
 
 module.exports = {
@@ -15,6 +17,8 @@ module.exports = {
       plugins: ['theme-helpers'],
       rules: {
         'theme-helpers/no-unknown-theme-color': 2,
+        'theme-helpers/no-unknown-theme-font-size': 2,
+        'theme-helpers/no-unknown-theme-font-weight': 2,
       },
     },
   },
--- tests/lib/rules/no-unknown-theme-color.test.js
+++ tests/lib/rules/no-unknown-theme-color.test.js
@@ -1,20 +1,25 @@
 const RuleTester = require('eslint').RuleTester
 const rule = require('../../../lib/rules/no-unknown-theme-color')
 
+const fakeColors = ["darkGray"]
+
 const ruleTester = new RuleTester()
 
 ruleTester.run('no-unknown-theme-color', rule, {
   valid: [
     {
       code: "themeColor('darkGray');",
+      options: [{colors: fakeColors}]
     },
     {
       code: 'themeColor(colorName);',
+      options: [{colors: fakeColors}]
     },
   ],
   invalid: [
     {
       code: "themeColor('darGray');",
+      options: [{colors: fakeColors}],
       errors: [
         {
           message: "Unknown theme color 'darGray'",
