a6ee8ea89429a22a42f691cb097fd668fe39e98b
Antonio Juan Sánchez <antonio.juan@elparking.com>
2019-09-20 16:20:06 +0200

    wip

--- index.js
+++ index.js
@@ -2,18 +2,35 @@ const allRules = {
   'no-unknown-theme-color': require('./lib/rules/no-unknown-theme-color'),
   'no-unknown-theme-font-size': require('./lib/rules/no-unknown-theme-font-size'),
   'no-unknown-theme-font-weight': require('./lib/rules/no-unknown-theme-font-weight'),
+  'jsx-no-literals': require('./lib/rules/jsx-no-literals'),
 }
 
 module.exports = {
   rules: allRules,
   configs: {
+    recommended: {
+      plugins: [
+        'react'
+      ],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+    },
     all: {
       plugins: ['theme-helpers'],
       rules: {
+        'theme-helpers/jsx-no-literals': 1,
         'theme-helpers/no-unknown-theme-color': 2,
         'theme-helpers/no-unknown-theme-font-size': 2,
         'theme-helpers/no-unknown-theme-font-weight': 2,
       },
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      }
     },
   },
 }
