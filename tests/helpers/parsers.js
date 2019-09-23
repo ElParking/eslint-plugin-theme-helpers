23bdcf343fbbafba653d97bfbeb8dbfa5b75cd50
Antonio Juan Sánchez <antonio.juan@elparking.com>
2019-09-23 18:19:41 +0200

    Remove babel-eslint

--- tests/helpers/parsers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-const NODE_MODULES = '../../node_modules';
-
-module.exports = {
-  BABEL_ESLINT: path.join(__dirname, NODE_MODULES, 'babel-eslint'),
-};
